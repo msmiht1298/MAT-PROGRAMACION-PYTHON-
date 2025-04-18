2fb4a4266ed67d2c371039273d98db5f103150be
msmiht1298 <m.djesus12b@gmail.com>
2025-04-18 11:10:23 -0400

    calculadoraconbiblioteca.py

---
+++ practica2/calculadoraconbibliotecas.py
@@ -0,0 +1,7 @@
+import math
+
+# Ejemplo de operaciones con la biblioteca math
+numero = float(input("Ingresa un número: "))
+
+print("Raíz cuadrada:", math.sqrt(numero))
+print("Potencia (elevado al cuadrado):", math.pow(numero, 2))
